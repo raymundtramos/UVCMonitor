02fee8dced22763ab3922fa79df44d3561bf3bb2
raymundtramos <raymundtheodoreramos@gmail.com>
2020-08-06 19:56:11 -0700

    Implemented the flow of how the Preview Settings should update when options are changed.

--- app/src/main/java/com/raymund/uvcmonitor/SettingsActivity.kt
+++ app/src/main/java/com/raymund/uvcmonitor/SettingsActivity.kt
@@ -1,14 +1,15 @@
 package com.raymund.uvcmonitor
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.DropDownPreference
-import androidx.preference.PreferenceCategory
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
+import androidx.preference.Preference.OnPreferenceChangeListener
 import com.serenegiant.usb.UVCSize
 
 class SettingsActivity : AppCompatActivity() {
-    private var mSupportedSizeList : UVCSize? = null;
+    private var mSupportedSizeList: UVCSize? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +22,40 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment() : PreferenceFragmentCompat() {
+    class SettingsFragment() : PreferenceFragmentCompat(), OnPreferenceChangeListener {
         private var mSupportedSizeList: UVCSize? = null;
+        private var mPreviewFrameFormat: ListPreference? = null;
+        private var mPreviewResolution: ListPreference? = null;
+        private var mPreviewFramerate: ListPreference? = null;
 
-        constructor(supportedSizeList: UVCSize) : this(){
+        constructor(supportedSizeList: UVCSize) : this() {
             mSupportedSizeList = supportedSizeList;
+        }
+
+        private fun createPreviewPref(
+            context: Context,
+            _key: String,
+            _title: String,
+            _entries: Array<CharSequence>
+        ): ListPreference {
+            return ListPreference(context).apply {
+                onPreferenceChangeListener = this@SettingsFragment
+                key = _key
+                title = _title
+                entries = _entries
+                entryValues = entries
+                summary = entries[0]
+                setValueIndex(0)
+            }
+        }
+
+        private fun updatePreviewPref(preference: ListPreference?, _entries: Array<CharSequence>) {
+            preference!!.apply {
+                entries = _entries
+                entryValues = entries
+                summary = entries[0]
+                setValueIndex(0)
+            }
         }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -37,38 +67,70 @@ class SettingsActivity : AppCompatActivity() {
             val categoryPreview = PreferenceCategory(context).apply {
                 title = "Preview Settings"
             }
-            val previewFrameFormat = DropDownPreference(context).apply {
-                key = "preview_frame_format"
-                title = "Frame Format"
-                entries = mSupportedSizeList!!.formatCharSeq
-                entryValues =  mSupportedSizeList!!.formatCharSeq
-                summary = mSupportedSizeList!!.formatCharSeq[0]
-            }
-            val previewResolution = DropDownPreference(context).apply {
-                key = "preview_resolution"
-                title = "Resolution"
-                entries = format.frameResCharSeq
-                entryValues =  format.frameResCharSeq
-                summary = format.frameResCharSeq[0]
-            }
-            val previewFramerate = DropDownPreference(context).apply {
-                key = "preview_framerate"
-                title = "Framerate"
-                entries = frame.intervalCharSeq
-                entryValues =  frame.intervalCharSeq
-                summary = frame.intervalCharSeq[0]
-            }
+            mPreviewFrameFormat = createPreviewPref(
+                context,
+                "preview_frame_format",
+                "Frame Format",
+                mSupportedSizeList!!.formatCharSeq
+            )
+            mPreviewResolution = createPreviewPref(
+                context,
+                "preview_resolution",
+                "Resolution",
+                format.frameResCharSeq
+            )
+            mPreviewFramerate = createPreviewPref(
+                context,
+                "preview_framerate",
+                "Framerate",
+                frame.intervalCharSeq
+            )
 
             val categoryRecord = PreferenceCategory(context).apply {
                 title = "Record Settings"
             }
 
             screen.addPreference(categoryPreview)
-            screen.addPreference(previewFrameFormat)
-            screen.addPreference(previewResolution)
-            screen.addPreference(previewFramerate)
+            screen.addPreference(mPreviewFrameFormat)
+            screen.addPreference(mPreviewResolution)
+            screen.addPreference(mPreviewFramerate)
             screen.addPreference(categoryRecord)
             preferenceScreen = screen;
+        }
+
+        override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+            val key = preference!!.key
+            val index = (preference as ListPreference?)!!.findIndexOfValue(newValue as String);
+
+            if (key == "preview_frame_format") {
+                val format = mSupportedSizeList!!.getFormat(index);
+                val frame = format!!.getFrame(0);
+
+                preference.apply {
+                    summary = newValue as CharSequence?
+                }
+                updatePreviewPref(mPreviewResolution, format!!.frameResCharSeq)
+                updatePreviewPref(mPreviewFramerate, frame!!.intervalCharSeq)
+                return true
+            } else if (key == "preview_resolution") {
+                val formatValue = mPreviewFrameFormat!!.value
+                val formatIndex = mPreviewFrameFormat!!.findIndexOfValue(formatValue)
+                val format = mSupportedSizeList!!.getFormat(formatIndex)
+                val frame = format.getFrame(index);
+
+                preference.apply {
+                    summary = newValue as CharSequence?
+                }
+                updatePreviewPref(mPreviewFramerate, frame!!.intervalCharSeq)
+                return true
+            } else if (key == "preview_framerate") {
+                preference.apply {
+                    summary = newValue as CharSequence?
+                }
+                return true
+            } else {
+                return false
+            }
         }
     }
 }
